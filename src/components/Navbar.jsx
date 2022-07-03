9b4ca226fdb2c9c2c4c48473b015470b11baa22d
Mabast <mabast.niga@yahoo.com>
2022-07-03 02:32:21 -0500

    styling and icon changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
-import { BsChatLeft } from "react-icons/bs";
-import { RiNotification3Line } from "react-icons/ri";
+import { BiMessageSquareDots, BiBell } from "react-icons/bi";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import ReactToolTip from "react-tooltip";
 import { Link } from "react-router-dom";
@@ -9,7 +8,7 @@ import profile from "../data/profile.png";
 import logo from "../data/logo.jpg";
 import { Chat, Notification, UserProfile } from ".";
 
-const NavButton = ({ title, customFun, icon, color, dotColor }) => (
+const NavButton = ({ title, customFun, icon, color, dotColor, text }) => (
   <>
     <button
       type="button"
@@ -20,10 +19,11 @@ const NavButton = ({ title, customFun, icon, color, dotColor }) => (
     >
       <span
         style={{ background: color }}
-        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        className="absolute inline-flex items-center justify-center rounded-full h-3 w-3 right-1 top-1 p-2"
       >
-        {icon}
+        <p className="text-sm text-gray-100">{text}</p>
       </span>
+      {icon}
     </button>
     <ReactToolTip effect="solid" />
   </>
@@ -34,7 +34,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="flex justify-between p-2 md:px-7 relative h-16 bg-white">
+      <div className="flex justify-between p-2 md:px-7 relative h-16 bg-white shadow-sm">
         <Link to="./">
           <img src={logo} alt="logo" className="-my-2 w-20" />
         </Link>
@@ -44,28 +44,30 @@ const Navbar = () => {
             customFunc={() => handleClick("chat")}
             color="black"
             dotColor="black"
-            icon={<BsChatLeft />}
+            icon={<BiMessageSquareDots />}
+            text="4"
           />
           <NavButton
             title="Notification"
             customFunc={() => handleClick("notification")}
             color="black"
             dotColor="black"
-            icon={<RiNotification3Line />}
+            icon={<BiBell />}
+            text="7"
           />
           <div
             className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
             onClick={() => handleClick("userProfile")}
             data-tip="Profile"
           >
-            <img src={profile} className="rounded-full w-8 h-8" alt="avatar" />
+            <img src={profile} className="rounded-full w-9 h-9" alt="avatar" />
             <p>
-              <span className="text-gray-400 text-14">Hi, </span>
-              <span className="text-gray-400 font-bold ml-1 text-14">
+              <span className="text-gray-400 text-16">Hi, </span>
+              <span className="text-gray-400 font-bold ml-1 text-16">
                 Mabast
               </span>
             </p>
-            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+            <MdKeyboardArrowDown className="text-gray-400 text-16" />
           </div>
           <ReactToolTip place="Bottom" />
         </div>

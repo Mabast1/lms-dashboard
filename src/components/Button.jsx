5ffdca637e614e7aa5ab12a31d0eb87891afa36d
Mabast <mabast.niga@yahoo.com>
2022-07-04 23:53:17 -0500

    created button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+import { useStateContext } from "../context/ContextProvider";
+
+const Button = ({ bgColor, color, size, text, borderRadius, icon, width }) => {
+  const { setIsClicked } = useStateContext();
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl w-${width}`}
+      onClick={() => setIsClicked(false)}
+    >
+      {icon}
+      {text}
+    </button>
+  );
 };
 
 export default Button;
